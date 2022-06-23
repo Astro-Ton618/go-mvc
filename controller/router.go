a5421397b0503f53818d6e528d382144efe675e0
Massimiliano Falsini <falsinimassimiliano@gmail.com>
2022-06-23 20:28:36 +0200

    fixed SEO

--- controller/router.go
+++ controller/router.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"fmt"
-
 	"go_mvc/view"
 
 	"github.com/valyala/fasthttp"
@@ -11,8 +10,11 @@ import (
 func Router(ctx *fasthttp.RequestCtx) {
 	switch string(ctx.Path()) {
 	case "/":
-		fmt.Fprintf(ctx, "%s", view.Hello("Pippo"))
+		fmt.Fprintf(ctx, "%s", view.Home("go_mvc", "go_mvc"))
+		ctx.SetContentType("text/html; charset=utf-8")
+	case "/robots.txt":
+		fmt.Fprintf(ctx, "user-agent: * allow: /")
 	default:
-		fmt.Fprintf(ctx, "Do not misunderstand it, %q is not power of your creation! ", ctx.RequestURI())
+		fmt.Fprintf(ctx, "Do not misunderstand it, %q is not power of your creation!", ctx.RequestURI())
 	}
 }
--- go.mod
+++ go.mod
@@ -8,5 +8,5 @@ require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/klauspost/compress v1.15.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/quicktemplate v1.7.0 // indirect
+	github.com/valyala/quicktemplate v1.7.0
 )
